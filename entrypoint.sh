e8322543014a1607f7719d1ea45f34cf1573268d
Rahal Hamza <hamzarahal031@gmail.com>
2020-07-13 20:36:08 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@
 #4 target
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n\tUserKnownHostsFile=/dev/null" > ~/.ssh/config
 echo "$1" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 scp -r $2 $3:$4
