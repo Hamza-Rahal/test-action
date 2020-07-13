639807b63b7d640ad725316d187b71e339a12420
Rahal Hamza <hamzarahal031@gmail.com>
2020-07-13 19:56:14 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 #1 private key
+#2 source
+#3 host
+#4 target
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 echo "$1" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+scp -r $2 $3:$4
